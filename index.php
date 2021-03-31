06bda4b253978aaf27addf524bbc41546a496db8
lotusandfish <70317796+lotusandfish@users.noreply.github.com>
2021-03-31 23:47:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<h1>Hello World<h1>
+<?php 
+	$conn_string = "host=ec2-54-145-102-149.compute-1.amazonaws.com port=5432 dbname=d1tv9knvpdnlv1 user=gbvkqgmomzactx password=0ee20d0250395d4060ae69886de9e408dcb6801fee8ce79660b592699f0a3667";
+	$conn = pg_connect($conn_string);
+	  	if($conn){
+	      	echo "Good";
+	  	}
+ ?>
