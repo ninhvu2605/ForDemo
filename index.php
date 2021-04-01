ac55c628b2281b1bdc364f0cd8c2ef52b1f10de3
lotusandfish <70317796+lotusandfish@users.noreply.github.com>
2021-04-01 11:10:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+	session_start();
 	$conn_string = "host=ec2-23-21-229-200.compute-1.amazonaws.com port=5432 dbname=d45cnbrd3f2d7l user=qqhhlmrdigauql password=637e31a7084d8a47a567ba0f2fb9d51707920f4751f53b734bb438a6485f1395";
 	$conn = pg_connect($conn_string);
 	if(isset($_POST['login'])){
