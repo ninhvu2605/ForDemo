37b815e96402793c3a9385a265db65ac2092479e
lotusandfish <70317796+lotusandfish@users.noreply.github.com>
2021-04-01 09:14:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-	$conn_string = "host=ec2-54-145-102-149.compute-1.amazonaws.com port=5432 dbname=d1tv9knvpdnlv1 user=gbvkqgmomzactx password=0ee20d0250395d4060ae69886de9e408dcb6801fee8ce79660b592699f0a3667";
+	$conn_string = "host=ec2-23-21-229-200.compute-1.amazonaws.com port=5432 dbname=d45cnbrd3f2d7l user=qqhhlmrdigauql password=637e31a7084d8a47a567ba0f2fb9d51707920f4751f53b734bb438a6485f1395";
 	$conn = pg_connect($conn_string);
 	  	if($conn){
 	      	echo "Good";
