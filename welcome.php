5553f7e1a32c5787b9350205321e31d74042be28
lotusandfish <70317796+lotusandfish@users.noreply.github.com>
2021-04-01 11:11:04 +0700

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,6 +1,7 @@
 <?php 
+	session_start();
 	$conn_string = "host=ec2-23-21-229-200.compute-1.amazonaws.com port=5432 dbname=d45cnbrd3f2d7l user=qqhhlmrdigauql password=637e31a7084d8a47a567ba0f2fb9d51707920f4751f53b734bb438a6485f1395";
 	$conn = pg_connect($conn_string);
-	echo "Login Successfully";
+	echo "Login Successfully <br>";
 	echo "Welcome".$_SESSION['user'];
  ?>
