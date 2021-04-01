5cef749870ba75a7d164e56b10760ec50d6c0c6a
lotusandfish <70317796+lotusandfish@users.noreply.github.com>
2021-04-01 09:29:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php 
 	$conn_string = "host=ec2-23-21-229-200.compute-1.amazonaws.com port=5432 dbname=d45cnbrd3f2d7l user=qqhhlmrdigauql password=637e31a7084d8a47a567ba0f2fb9d51707920f4751f53b734bb438a6485f1395";
 	$conn = pg_connect($conn_string);
-	$sql = "SELECT * FROM tbl_user";
-	$query = pg_query($conn,$sql);
 	if($query){
-		echo "Good"
+		echo "Hello";
 	}
  ?>
