ba8c878bbf526f210bbc0d93fbf6518246f55c40
lotusandfish <70317796+lotusandfish@users.noreply.github.com>
2021-04-08 00:27:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
-<!--A Design by W3layouts
-   Author: W3layout
-   Author URL: http://w3layouts.com
-   License: Creative Commons Attribution 3.0 Unported
-   License URL: http://creativecommons.org/licenses/by/3.0/
-   -->
+<?php 
+   include('header.php');
+?>
          <!-- Slideshow 4 -->
          <div class="slider text-center">
             <div class="callbacks_container">
