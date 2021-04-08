379a29ef69f433883ab5d7c7ba7ddaad32ad6891
lotusandfish <70317796+lotusandfish@users.noreply.github.com>
2021-04-09 02:43:47 +0700

    Update single.php

--- single.php
+++ single.php
@@ -222,7 +222,7 @@
                   <div class="clearfix"> </div>
 		  <div class="toys single-item singlepage">
 		     <form action="cart.php" method="post">
-			  <input type="hidden" name="id" value="<?php echo $row1['product_id'] ?>">
+			  <input type="hidden" name="id" value="<?php echo $row1['product_id']; ?>">
 			  <label style="font-family: sans-serif; font-size: 18px; color:#EF7F9B;">Quantity:   </label>
 			  <input type="number" name="quantity" min="1">
                      	  <button class="top_toys_cart" type="submit" name="submit" value="">
