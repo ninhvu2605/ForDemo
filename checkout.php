6e7b5479aa779e6c3a143cec837fb1dd24428972
lotusandfish <70317796+lotusandfish@users.noreply.github.com>
2021-04-09 00:47:18 +0700

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -61,31 +61,16 @@
                      <h1><a class="navbar-brand" href="index.php">Toys-Shop</a></h1>
                   </div>
                   <div class="col-lg-5 col-md-6 search-right">
-                     <form class="form-inline my-lg-0">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search">
-                        <button class="btn" type="submit">Search</button>
+                     <?php
+                       if(isset($_POST['search_btn'])){
+                           $s = $_POST['search_input']; 
+                           header("Location:search.php?searchkey=$s");
+                       }
+                     ?>    
+                     <form method="post" class="form-inline my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" name = "search_input">
+                        <button class="btn" type="submit" name ="search_btn">Search</button>
                      </form>
-                  </div>
-                  <div class="col-lg-4 col-md-3 right-side-cart">
-                     <div class="cart-icons">
-                        <ul>
-                           <li>
-                              <span class="far fa-heart"></span>
-                           </li>
-                           <li>
-                              <button type="button" data-toggle="modal" data-target="#exampleModal"> <span class="far fa-user"></span></button>
-                           </li>
-                           <li class="toyscart toyscart2 cart cart box_1">
-                              <form action="#" method="post" class="last">
-                                 <input type="hidden" name="cmd" value="_cart">
-                                 <input type="hidden" name="display" value="1">
-                                 <button class="top_toys_cart" type="submit" name="submit" value="">
-                                 <span class="fas fa-cart-arrow-down"></span>
-                                 </button>
-                              </form>
-                           </li>
-                        </ul>
-                     </div>
                   </div>
                </div>
             </div>
@@ -108,14 +93,18 @@
                         <a href="shop.php" class="nav-link">Shop Now</a>
                      </li>
                      <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Product
                         </a>
+         
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                           <a class="nav-link" href="product.html">Kids Toys</a>
-                           <a class="nav-link " href="product.html">Dolls</a>
-                           <a class="nav-link " href="product.html">Key Toys</a>
-                           <a class="nav-link " href="product.html">Boys Toys</a>
+                           <?php
+                              $sql = "SELECT * FROM category";
+                              $query = pg_query($conn,$sql);
+                              while($row = pg_fetch_array($query)){ 
+                           ?>
+                           <a class="nav-link" href="product.php"><?php echo $row['cat_name']; ?></a> 
+                           <?php } ?>
                         </div>
                      </li>
                      <li class="nav-item">
@@ -125,7 +114,7 @@
                </div>
             </nav>
          </div>
-	</div>
+      </div>
          <!--//headder-->
          <!-- banner -->
          <div class="inner_page-banner one-img">
