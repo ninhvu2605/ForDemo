a481451cdeef2d144fd46016c8416a74abebb7e5
lotusandfish <70317796+lotusandfish@users.noreply.github.com>
2021-04-09 03:44:23 +0700

    Update cart.php

--- cart.php
+++ cart.php
@@ -2,8 +2,8 @@
    session_start();
    include('connect.php');
    if(isset($_GET['del'])){
-   unset($_SESSION['cart'][$_GET['del']]);
-   header('location:cart.php');
+      unset($_SESSION['cart'][$_GET['del']);
+      header('location:cart.php');
    }
    $totalAmount = 0;
    $amountNew = null;
@@ -12,7 +12,7 @@
      $q = pg_query($conn, "SELECT * FROM product WHERE product_id = $id");
      $product = pg_fetch_array($q);
      $_SESSION['cart'][$id] = $product;
-     $_SESSION['cart'][$id]['quantity']++; 
+     $_SESSION['cart'][$id]['quantity'] = $_SESSION['cart'][$id]['quantity'] + 1; 
    }
  ?>
 <!DOCTYPE html>
