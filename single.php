f85c0c50fef5499088362dc482f1b9b674970790
lotusandfish <70317796+lotusandfish@users.noreply.github.com>
2021-04-08 23:11:05 +0700

    Update single.php

--- single.php
+++ single.php
@@ -235,15 +235,24 @@
                         <ul class="resp-tabs-list">
                            <li>Description</li>
                            <li>Reviews</li>
+                           <li>Information</li>
                         </ul>
                         <div class="resp-tabs-container">
                            <!--/tab_one-->
                            <div class="tab1">
                               <div class="single_page">
-                                 <p>
-
+                                 <h6>Lorem ipsum dolor sit amet</h6>
+                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elPellentesque vehicula augue eget nisl ullamcorper, molestie
+                                    blandit ipsum auctor. Mauris volutpat augue dolor.Consectetur adipisicing elit, sed do eiusmod tempor incididunt
+                                    ut lab ore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. labore et dolore
+                                    magna aliqua.
                                  </p>
-                                </div>
+                                 <p class="para">Lorem ipsum dolor sit amet, consectetur adipisicing elPellentesque vehicula augue eget nisl ullamcorper, molestie
+                                    blandit ipsum auctor. Mauris volutpat augue dolor.Consectetur adipisicing elit, sed do eiusmod tempor incididunt
+                                    ut lab ore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. labore et dolore
+                                    magna aliqua.
+                                 </p>
+                              </div>
                            </div>
                            <!--//tab_one-->
                            <div class="tab2">
@@ -258,13 +267,44 @@
                                              <li><a href="#">Admin</a></li>
                                              <li><a href="#"><i class="fa fa-reply-all" aria-hidden="true"></i> Reply</a></li>
                                           </ul>
-                                          <p>
-                                          	<?php echo $row['product_name']; ?>
+                                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elPellentesque vehicula augue eget.Ut enim ad minima veniam,
+                                             quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis
+                                             autem vel eum iure reprehenderit.
                                           </p>
                                        </div>
                                        <div class="clearfix"> </div>
                                     </div>
+                                    <div class="add-review">
+                                       <h4>add a review</h4>
+                                       <form action="#" method="post">
+                                          <div class="row">
+                                             <div class="col-md-6">
+                                                <input type="text" name="Name" required="">
+                                             </div>
+                                             <div class="col-md-6">
+                                                <input type="email" name="Email" required="">
+                                             </div>
+                                          </div>
+                                          <textarea name="Message" required=""></textarea>
+                                          <input type="submit" value="SEND">
+                                       </form>
+                                    </div>
                                  </div>
+                              </div>
+                           </div>
+                           <div class="tab3">
+                              <div class="single_page">
+                                 <h6>Teddy Bear(Blue)</h6>
+                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elPellentesque vehicula augue eget nisl ullamcorper, molestie
+                                    blandit ipsum auctor. Mauris volutpat augue dolor.Consectetur adipisicing elit, sed do eiusmod tempor incididunt
+                                    ut lab ore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. labore et dolore
+                                    magna aliqua.
+                                 </p>
+                                 <p class="para">Lorem ipsum dolor sit amet, consectetur adipisicing elPellentesque vehicula augue eget nisl ullamcorper, molestie
+                                    blandit ipsum auctor. Mauris volutpat augue dolor.Consectetur adipisicing elit, sed do eiusmod tempor incididunt
+                                    ut lab ore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. labore et dolore
+                                    magna aliqua.
+                                 </p>
                               </div>
                            </div>
                         </div>
