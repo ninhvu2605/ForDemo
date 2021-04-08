33bcab2553869a1d5ac69a544e5046561276fb01
lotusandfish <70317796+lotusandfish@users.noreply.github.com>
2021-04-09 05:17:44 +0700

    Update order.php

--- order.php
+++ order.php
@@ -10,7 +10,7 @@ if(isset($_POST['check-out'])){
 	$name = $_POST['name'];
 	$address = $_POST['address'];
 	$phone = $_POST['phone'];
-	$total_amount = $_POST['total_amount'];
+	$total_amount = $_SESSION['total_amount'];
 	$pay = $_POST['pay'];
 	$time = current_timestamp();
 	
@@ -49,7 +49,45 @@ if(isset($_POST['check-out'])){
 
 ob_flush();
 ?>
-<style type="text/css">
+<!DOCTYPE html>
+<html lang="zxx">
+   <head>
+      <title>Toys Shop an Ecommerce Category Bootstrap Responsive Web Template | Home :: w3layouts</title>
+      <!--meta tags -->
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="keywords" content="Toys Shop Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+         Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+      <script>
+         addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+         }, false);
+         
+         function hideURLbar() {
+            window.scrollTo(0, 1);
+         }
+      </script>
+      <!--//meta tags ends here-->
+      <!--booststrap-->
+      <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+      <!--//booststrap end-->
+      <!-- font-awesome icons -->
+      <link href="css/fontawesome-all.min.css" rel="stylesheet" type="text/css" media="all">
+      <!-- //font-awesome icons -->
+      <!-- For Clients slider -->
+      <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="all" />
+      <!--flexs slider-->
+      <link href="css/JiSlider.css" rel="stylesheet">
+      <!--Shoping cart-->
+      <link rel="stylesheet" href="css/shop.css" type="text/css" />
+      <!--//Shoping cart-->
+      <!--stylesheets-->
+      <link href="css/style.css" rel='stylesheet' type='text/css' media="all">
+      <!--//stylesheets-->
+      <link href="//fonts.googleapis.com/css?family=Sunflower:500,700" rel="stylesheet">
+      <link href="//fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+   </head>
+	<style type="text/css">
  	input{
  		color:#FF0094;
  		background:#343A40;
@@ -78,6 +116,103 @@ ob_flush();
 		width: 70%;
 	}
 </style>
+   <body>
+   <div class="header-outs" id="home">
+         <div class="header-bar">
+            <div class="info-top-grid">
+               <div class="info-contact-agile">
+                  <ul>
+                     <li>
+                        <span class="fas fa-phone-volume"></span>
+                        <p>+(000)123 4565 32</p>
+                     </li>
+                     <li>
+                        <span class="fas fa-envelope"></span>
+                        <p><a href="mailto:info@example.com">info@example1.com</a></p>
+                     </li>
+                     <li>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+            <div class="container-fluid">
+               <div class="hedder-up row">
+                  <div class="col-lg-3 col-md-3 logo-head">
+                     <h1><a class="navbar-brand" href="index.php">Toys-Shop</a></h1>
+                  </div>
+                  <div class="col-lg-5 col-md-6 search-right">
+                     <?php
+                       if(isset($_POST['search_btn'])){
+                           $s = $_POST['search_input']; 
+                           header("Location:search.php?searchkey=$s");
+                       }
+                     ?>    
+                     <form method="post" class="form-inline my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" name = "search_input">
+                        <button class="btn" type="submit" name ="search_btn">Search</button>
+                     </form>
+                  </div>
+               </div>
+            </div>
+            <nav class="navbar navbar-expand-lg navbar-light">
+               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+               </button>
+               <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                  <ul class="navbar-nav ">
+                     <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                     </li>
+                     <li class="nav-item">
+                        <a href="about.html" class="nav-link">About</a>
+                     </li>
+                     <li class="nav-item">
+                        <a href="service.html" class="nav-link">Service</a>
+                     </li>
+                     <li class="nav-item">
+                        <a href="shop.php" class="nav-link">Shop Now</a>
+                     </li>
+                     <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Product
+                        </a>
+         
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                           <?php
+                              $sql2 = "SELECT * FROM category";
+                              $query2 = pg_query($conn,$sql2);
+                              while($row2 = pg_fetch_array($query2)){ 
+                           ?>
+                           <a class="nav-link" href="search.php?searchkey=<?php echo $row2['cat_name']; ?>"><?php echo $row2['cat_name']; ?></a> 
+                           <?php } ?>
+                        </div>
+                     </li>
+                     <li class="nav-item">
+                        <a href="contact.html" class="nav-link">Contact</a>
+                     </li>
+                  </ul>
+               </div>
+            </nav>
+         </div>
+      </div>
+      <!--//headder-->
+      <!-- banner -->
+      <div class="inner_page-banner one-img">
+      </div>
+      <!--//banner -->
+      <!-- short -->
+      <div class="using-border py-3">
+         <div class="inner_breadcrumb  ml-4">
+            <ul class="short_ls">
+               <li>
+                  <a href="index.html">Home</a>
+                  <span>/ /</span>
+               </li>
+               <li>Cart</li>
+            </ul>
+         </div>
+      </div>
+
 
 <div class="row">
   	<div class="col-12">
